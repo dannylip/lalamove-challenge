79511dfac729a9e4b51aba5a90002d8cac9c9419
Danny LIP <>
2018-08-06 23:32:02 +0800

    Initialize xcode project and Mock API

---
+++ lalamoveChallenge/lalamoveChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lalamoveChallenge
+//
+//  Created by Danny LIP on 6/8/2018.
+//  Copyright © 2018年 Danny LIP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
